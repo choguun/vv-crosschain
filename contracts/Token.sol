959952b7cf93dad17e857c578d7764f44c5e4b6e
choguun <29241145+choguun@users.noreply.github.com>
2024-09-23 00:08:28 +0700

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,17 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Profile} from "./Profile.sol";
 
-contract Token is ERC20, Ownable {
+import {EquitoApp} from "../lib/equito/src/EquitoApp.sol";
+import {bytes64, EquitoMessage, EquitoMessageLibrary} from "../lib/equito/src/libraries/EquitoMessageLibrary.sol";
+
+contract Token is ERC20, EquitoApp {
     address public profile;
     address public world;
 
-    constructor(address _initialOwner, address _world, address _profile) ERC20("CUBE Token", "CUBE") Ownable(_initialOwner) {
+    constructor(address _initialOwner, address _world, address _profile, address _router) ERC20("CUBE Token", "CUBE") EquitoApp(_router) {
         profile = _profile;
         world = _world;
+    }
+
+     /// @notice Sends a cross-chain message using Equito.
+    /// @param receiver The address of the receiver on the destination chain.
+    /// @param destinationChainSelector The identifier of the destination chain.
+    /// @param amount The amount of tokens to send.
+    function crossChainTransfer(
+        bytes64 calldata receiver,
+        uint256 destinationChainSelector,
+        uint256 amount
+    ) external payable {
+        _burn(msg.sender, amount);
+        bytes memory data = abi.encode(receiver, amount);
+        router.sendMessage{value: msg.value}(
+            getPeer(destinationChainSelector),
+            destinationChainSelector,
+            data
+        );
+    }
+
+    /// @notice Receives a cross-chain message from a peer.
+    ///         Mints the appropriate amount of tokens to the receiver address.
+    /// @param message The Equito message received.
+    /// @param messageData The data of the message received.
+    function _receiveMessageFromPeer(
+        EquitoMessage calldata message,
+        bytes calldata messageData
+    ) internal override {
+        (bytes64 memory receiver, uint256 amount) = abi.decode(
+            messageData,
+            (bytes64, uint256)
+        );
+        _mint(EquitoMessageLibrary.bytes64ToAddress(receiver), amount);
     }
 
     modifier onlyUser() {
